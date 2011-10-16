92c1d79efddd0c474241830fe44d18f54869b0e0
aodin <aodin@hyrax.(none)>
2011-10-16 01:04:07 -0700

    bu fixes for staff

--- city_aware/staff/urls.py
+++ city_aware/staff/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
 
     url(r'^/$', 'staff.views.staff_home', name='staff_home'),
-    url(r'^/event-categories$', 'staff.views.view_event_categories'),
+    url(r'^/event-categories$', 'staff.views.view_event_categories', name='view_event_categories'),
     url(r'^/venues$', 'staff.views.view_venues', name='view_venues'),
 )
