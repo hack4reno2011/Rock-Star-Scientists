0b4927efa1d1118cf4300ea1e1d54b2ea74afd5b
aodin <aodin@hyrax.(none)>
2011-10-16 00:56:10 -0700

    edits for event categories and venues

--- city_aware/staff/views.py
+++ city_aware/staff/views.py
@@ -5,3 +5,13 @@ def staff_home(request):
     #TODO direct to template
     a = {}
     return render(request, 'staff/staff_home.html', a)
+    
+def view_event_categories(request):
+    #TODO direct to template
+    a = {}
+    return render(request, 'staff/event_categories_main.html', a)
+    
+def view_venues(request):
+    #TODO direct to template
+    a = {}
+    return render(request, 'staff/venues_main.html', a)
